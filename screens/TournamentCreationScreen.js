46e498dc4b8efbc3156894512a906d09e6170b3e
clement perez <rezperez@gmail.com>
2018-05-03 19:04:11 -0700

    confirmation and navigate back after creating a tournament

--- screens/TournamentCreationScreen.js
+++ screens/TournamentCreationScreen.js
@@ -1,8 +1,9 @@
 import React, { Component }  from 'react';
 import PropTypes from 'prop-types';
-import { View, Button } from 'react-native';
-import { FormLabel, FormInput } from 'react-native-elements'
+import { View } from 'react-native';
+import { FormLabel, FormInput, Button } from 'react-native-elements'
 
+import DropdownAlert from 'react-native-dropdownalert';
 import { postTournament } from '../api/tournament';
 
 import { NavigationActions } from 'react-navigation';
@@ -42,32 +43,63 @@ componentWillReceiveProps(nextProps) {
 _createTournament = () => {
   console.log("Creating Tournament for " + this.state.tournamentName + " " + this.state.sportName);
 
+  this.dropdown.alertWithType('info', 'Great !', this.state.tournamentName + ' have been successfully created, good luck !.');
+
+/*
   postTournament(this.state.tournamentName, this.state.sportName)
     .then((response) => {
           console.log("Successfully Created Tournament for " + this.state.tournamentName + " " + this.state.sportName);
     })
     .catch((error) => {
       console.log('failed to create tournament ' + error);
-    }).done();
+    }).done();*/
 };
+
+onClose(data){
+
+const resetAction = NavigationActions.reset({
+  index: 0,
+  actions: [
+    NavigationActions.navigate({ routeName: 'Tournaments'})
+  ]
+})
+this.props.navigation.dispatch(resetAction)
+}
 
   render() {
 
     return (
-        <View style={{flex: 1}}>
-            <FormLabel> Tournament Title </FormLabel>
+        <View style={{flex: 1}} >
+            <FormLabel
+              labelStyle={{color: 'white'}}> Tournament Title </FormLabel>
             <FormInput onChangeText={(text) => {
                 this.setState({tournamentName:text});
             }}/>
-            <FormLabel> Sport </FormLabel>
+            <FormLabel
+              labelStyle={{color: 'white'}}> Sport </FormLabel>
             <FormInput onChangeText={(text) => {
                 this.setState({sportName:text});
             }}/>
-            <FormLabel> Description </FormLabel>
+            <FormLabel
+              labelStyle={{color: 'white'}}>
+              Description </FormLabel>
             <FormInput onChangeText={(text) => {
             }}/>
-            <Button title='Create Tournament'
-                      onPress={this._createTournament}/>
+            <Button
+                title='Create Tournament'
+                titleStyle={{ color: 'black' }}
+                onPress={this._createTournament}
+                buttonStyle={{
+                    backgroundColor: "tomato",
+                    marginTop: 16,
+                    height: 45,
+                    borderColor: "transparent",
+                    borderWidth: 0,
+                    borderRadius: 5
+                }}/>
+             <DropdownAlert
+                 ref={ref => this.dropdown = ref}
+                 onClose={data => this.onClose(data)}  />
         </View>
     );
   }
